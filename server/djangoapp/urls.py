09a8abce65763f1131bbaeb40596ae795b54f0db
benromaissae1 <benromaissae1@gmail.com>
2025-05-22 13:22:34 -0400

    new tasks

--- server/djangoapp/urls.py
+++ server/djangoapp/urls.py
@@ -5,9 +5,7 @@ from . import views
 
 app_name = 'djangoapp'
 urlpatterns = [
-    # # path for registration
-
-    # path for login
+    path(route='register', view=views.registration, name='registration'),
     path(route='login', view=views.login_user, name='login'),
     path(route='logout', view=views.logout_user, name='logout_user'),
     # path for dealer reviews view
