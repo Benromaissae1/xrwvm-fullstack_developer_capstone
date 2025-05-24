73b3877b50db66b6a0be8e99431c8182848e6c58
benromaissae1 <benromaissae1@gmail.com>
2025-05-24 17:21:39 -0400

    new changes

--- server/frontend/src/App.js
+++ server/frontend/src/App.js
@@ -3,6 +3,7 @@ import Dealer from "./components/Dealers/Dealer"
 import Register from "./components/Register/Register"
 import LoginPanel from "./components/Login/Login"
 import { Routes, Route } from "react-router-dom"
+import PostReview from "./components/Dealers/PostReview"
 
 function App() {
   return (
@@ -11,6 +12,7 @@ function App() {
       <Route path="/register" element={<Register />} />
       <Route path="/dealers" element={<Dealers/>} />
       <Route path="/dealer/:id" element={<Dealer/>} />
+      <Route path="/postreview/:id" element={<PostReview/>} />
     </Routes>
   );
 }
